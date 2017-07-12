837433102ad373337061d851ed1e3ac0bc6d200c
Kitty Pelerin <KPelerin78@gmail.com>
2017-07-11 21:40:16 -0400

    created game logic with modified main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,31 @@
-console.log ("Up and running");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+//arrray of cards
 
-console.log("User flipped " + cardTwo);
-console.log("User flipped " + cardFour);
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+
+//user flipped
+
+//card one
+
+var cardOne = cards [0];
+cardsInPlay.push(cardOne);
+
+console.log ("User flipped queen");
+
+//card two
+
+var cardTwo = cards [2];
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped king");
+
+//logic statement
+
+if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+     alert("You found a match!");
+  } else {
+     alert("Sorry, try again.");
+  }
+}
