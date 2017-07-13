a8d9d3eb2e457d5643fb9ee6e9a069fb565aa8d0
Kitty Pelerin <KPelerin78@gmail.com>
2017-07-12 23:42:38 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
 
 //arrray of cards
 
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = [
+    {rank: "queen",
+   suit: "hearts",
+   cardImage: "img/queen-of-hearts.png"
+  },
+    {rank:"queen",
+   suit: "diamonds",
+   cardImage: "img/queen-of-diamonds.png"
+  },
+    {rank: "king",
+   suit: "hearts",
+   cardImage: "img/king-of-hearts.png"
+   },
+    {rank: "king",
+   suit: "diamonds",
+   cardImage:"img/king-of-diamonds.png"
+  }
+];
 var cardsInPlay = [];
 
 //checking for match
@@ -19,10 +36,14 @@ var checkForMatch = function() {
 // flipCard function
 var flipCard = function(cardId) {
   console.log ("User flipped queen");
+  console.log("img/queen-of-hearts.png");
+  console.log("hearts");
   console.log("User flipped king");
+  console.log("img/king-of-hearts.png");
+  console.log("hearts");
   console.log("flipped over");
-  console.log ("User flipped queen " + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
+  console.log ("User flipped queen " + cards[cardId].rank);
+  cardsInPlay.push(cards[cardId].queen);
   checkForMatch();
 }
 flipCard(0);
