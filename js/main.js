2c237f7df7ebc3ff32422347ea282783eec28dd0
Kitty Pelerin <KPelerin78@gmail.com>
2017-07-12 23:04:08 -0400

    added js flipcard function

--- js/main.js
+++ js/main.js
@@ -4,28 +4,26 @@
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
 
-//user flipped
+//checking for match
 
-//card one
-
-var cardOne = cards [0];
-cardsInPlay.push(cardOne);
-
-console.log ("User flipped queen");
-
-//card two
-
-var cardTwo = cards [2];
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped king");
-
-//logic statement
-
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-     alert("You found a match!");
-  } else {
+var checkForMatch = function() {
+  if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You found a match!");
+    } else {
      alert("Sorry, try again.");
   }
+ }
 }
+
+// flipCard function
+var flipCard = function(cardId) {
+  console.log ("User flipped queen");
+  console.log("User flipped king");
+  console.log("flipped over");
+  console.log ("User flipped queen " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  checkForMatch();
+}
+flipCard(0);
+flipCard(2);
